b010208f6875fa6704028172f31b2bfb1915618c
Leonardo Galante <lnd@MacBook-Pro-de-Leonardo.local>
2016-09-29 02:17:32 -0300

    css minified, new polygon and lot of css changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,26 @@
 var gulp = require('gulp');
-var htmlmin = require('gulp-htmlmin');
 var rename = require('gulp-rename');
+var htmlmin = require('gulp-htmlmin');
+var cleanCSS = require('gulp-clean-css');
 
-gulp.task('minify', function() {
+
+gulp.task('minify-html', function () {
     return gulp.src('index-dev.html')
         .pipe(htmlmin({
-            collapseWhitespace: true,
-            minifyCSS: true
+            collapseWhitespace: true
         }))
         .pipe(rename('index.html'))
         .pipe(gulp.dest(''))
 });
 
-gulp.task('default', ['minify'], function() {
-    gulp.watch('index-dev.html', ['minify']);
+gulp.task('minify-css', function () {
+    return gulp.src('css/styles.css')
+        .pipe(cleanCSS())
+        .pipe(rename('styles.min.css'))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('default', ['minify-html', 'minify-css'], function () {
+    gulp.watch('index-dev.html', ['minify-html']);
+    gulp.watch('css/styles.css', ['minify-css']);
 })
