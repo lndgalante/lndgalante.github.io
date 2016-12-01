2c2386338e6cd096ba544bfe6bf1d44e1b66b915
Leonardo Galante <leo@fenwil.local>
2016-12-01 11:10:05 -0300

    aload removed, noopener removed, quote.png minified, script async

--- src/js/script.js
+++ src/js/script.js
@@ -1,33 +1,3 @@
-/**
- * aload - v1.2.2
- *
- * Copyright (c) 2016, @pazguille <guille87paz@gmail.com>
- * Released under the MIT license.
- */
-/**
- * Loads images, scripts, styles, iframes, videos and audios asynchronously.
- * @param {NodeList} [nodes] - A NodeList of elements. By default, it is the result of `querySelectorAll('[data-aload]')`.
- * @returns {NodeList}
- */
-function aload(nodes) {
-    'use strict';
-
-    var attribute = 'data-aload';
-
-    nodes = nodes || window.document.querySelectorAll('[' + attribute + ']');
-
-    if (nodes.length === undefined) {
-        nodes = [nodes];
-    }
-
-    [].forEach.call(nodes, function (node) {
-        node[node.tagName !== 'LINK' ? 'src' : 'href'] = node.getAttribute(attribute);
-        node.removeAttribute(attribute);
-    });
-
-    return nodes;
-}
-
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         define([], factory(root));
@@ -695,8 +665,6 @@ window.onload = function () {
         speed: 1500,
         easing: 'easeInOutQuad'
     })
-
-    aload();
 
     var polygon = document.getElementById("polygon");
     var arrow = document.getElementById("arrow");
