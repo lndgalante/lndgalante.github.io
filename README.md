4e37826f425990b10292bd2c9107f0e744927ecc
lndgalante <lndgalante@gmail.com>
2018-01-12 21:55:44 -0300

    New site 🎉

--- README.md
+++
@@ -1,8 +0,0 @@
-# leonardogalante.com
-My personal site
-
-Brand Icons: [https://simpleicons.org](simple-icons)
-
-Futuristic Icons: [http://ego-icons.com](ego-icons)
-
-Framework CSS: [http://bulma.io](bulma)
---
+++ app.js
@@ -0,0 +1,13 @@
+const getVisual = async () => {
+  const res = await fetch('https://archillect-api.now.sh/random')
+  const image = await res.json()
+  return image.source
+}
+
+const setHeaderBackground = async () => {
+  const background = await getVisual()
+  const header = document.querySelector('header')
+  header.style.backgroundImage = `url(${background})`
+}
+
+setHeaderBackground()
